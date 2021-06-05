0a08112a007e15fc120537acc8f441e16667ee19
rafsun <rafsun.dev@gmail.com>
2021-06-06 01:46:35 +0600

    Initial Commit: Rafsun's File

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World \nMohammad Rafsun Jany \n01718233341"); //creates new line;
+
+        System.out.println("Mohammad Rafsun; 015806445251\b"); //deletes only one digit or letter or space before it;
+
+        System.out.println("Mohammad Rafsun Jan \t01580644525"); //creates 5 blanks/spaces in output;
+        System.out.println("Mohammad Rafsun Jany \t01580644525"); //creates 4 blanks/spaces in output;
+        System.out.println("1 \t2"); //creates 3 blanks/spaces in output;
+
+        System.out.println("\"Java Developer\""); //helps to show double inverted comma in output;
+
+        System.out.println("\'Java Developer\'"); //helps to show single inverted comma in output;
+
+        System.out.println("\\rafsun's folder"); //helps to show backslash in output;
+
+        System.out.println("Hi rafsun, \rwhere are you going?"); //removes all previous texts or spaces before it;
+
+        System.out.println("Good Morning, \fHow are you?"); //used as page-break that means it starts a new page;
+    }
+}
